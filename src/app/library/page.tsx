3a3012ffb53632ca7362872d31298f78d06b9c0f
nathanielpeel <nathanjames719@gmail.com>
2023-09-16 16:53:31 -0700

    Configure colors in tailwind.config. Start creating login page.

---
+++ src/app/library/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Library = () => {
+  return (
+    <div className='bg-white'>Library</div>
+  )
+}
+
+export default Library
---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='bg-gradient-to-t from-yellow to-strawberry h-[100vh]'>
+      <h1>Welcome to Shelph</h1>
+    </div>
+  )
+}
+
+export default Login
